6cb5928b5839cfc24a9c508adf05bd8dbb40152f
edoardocenni01 <edoardocenni01@gmail.com>
2018-09-15 11:11:42 +0200

    2

---
+++ Casting/Casting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 55555;
+            Console.WriteLine($"il valore di a è {a.ToString()}");
+            short s = (short)a;
+            Console.WriteLine($"Il valore di s è {s.ToString()}");
+            Console.ReadLine();
+        }
+    }
+}
